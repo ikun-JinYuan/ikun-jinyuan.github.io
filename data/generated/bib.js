e285e5eda655483f39e5f5dfccc8f47945d6f40e
ikun-JinYuan <2172344991@qq.com>
2025-05-16 00:23:02 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,114 @@
+﻿define({ entries : {
+    "Carion2020End": {
+        "author": "Carion, Nicolas and Massa, Francisco and Synnaeve, Nicolas and Usunier, Alexandre and Kirillov, Alexander and Zagoruyko, Sergey",
+        "booktitle": "Computer Vision \u2013 ECCV 2020",
+        "doi": "10.1007/978-3-030-58452-8_13",
+        "keywords": "object detection, transformer, end-to-end, set prediction, CV",
+        "pages": "213--229",
+        "publisher": "Springer",
+        "series": "Lecture Notes in Computer Science",
+        "title": "End-to-End Object Detection with Transformers",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1007/978-3-030-58452-8_13",
+        "volume": "12346",
+        "year": "2020"
+    },
+    "Chen2024GMT": {
+        "author": "Chen, Fuhao and Tsaftaris, Sotirios A. and Giuffrida, Mario V.",
+        "journal": "arXiv preprint arXiv:2406.17109",
+        "keywords": "leaf segmentation, transformer, guided functions, plant phenotyping",
+        "title": "{GMT}: Guided Mask Transformer for Leaf Instance Segmentation",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2406.17109",
+        "year": "2024"
+    },
+    "Cheng2022Mask2Former": {
+        "author": "Cheng, Bowen and Misra, Ishan and Schwing, Alexander G. and Kirillov, Alexander and Girdhar, Rohit",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "doi": "10.1109/CVPR52688.2022.00133",
+        "keywords": "panoptic segmentation, instance segmentation, masked attention, universal framework",
+        "pages": "1280--1289",
+        "series": "CVPR",
+        "title": "Masked-attention Mask Transformer for Universal Image Segmentation (Mask2Former)",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1109/CVPR52688.2022.00133",
+        "year": "2022"
+    },
+    "Fang2023BAFNet": {
+        "author": "Fang, Jiawei and Jiang, Haifeng and Zhang, Shuo and Sun, Ling and Hu, Xiaoliang and Liu, Jie and Gong, Meng and Liu, Huan and Fu, Yanning",
+        "doi": "10.3389/fpls.2023.1123410",
+        "journal": "Frontiers in Plant Science",
+        "keywords": "bidirectional attention, fusion network, pepper leaf, CNN-transformer",
+        "pages": "1123410",
+        "series": "Frontiers",
+        "title": "BAF-Net: Bidirectional Attention Fusion Network via CNN and Transformers for Pepper Leaf Segmentation",
+        "type": "article",
+        "url": "https://doi.org/10.3389/fpls.2023.1123410",
+        "volume": "14",
+        "year": "2023"
+    },
+    "Gomes2020Leaf": {
+        "author": "Gomes, Daniel P. S. and Zheng, Ying",
+        "journal": "arXiv preprint arXiv:2012.11486",
+        "keywords": "leaf counting, uncertainty estimation, test-time augmentation, deconvolutional networks",
+        "title": "Leaf Segmentation and Counting with Deep Learning: On Model Certainty, Test-Time Augmentation, Trade-Offs",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2012.11486",
+        "year": "2020"
+    },
+    "Lin2022SelfSupervised": {
+        "author": "Lin, Xiang and Li, Che-Tsung and Adams, Samuel and Kouzani, Abbas and Jiang, Ruijia and He, Lu and Hu, Yu and Vernon, Mike and Doeven, Elodie and Webb, Laura and McClellan, Taylor and Guskic, Adnan",
+        "doi": "10.1016/j.patcog.2022.109021",
+        "journal": "Pattern Recognition",
+        "keywords": "self-supervised learning, leaf segmentation, complex lighting, contrastive learning",
+        "pages": "109021",
+        "series": "Elsevier",
+        "title": "Self-Supervised Leaf Segmentation under Complex Lighting Conditions",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.patcog.2022.109021",
+        "volume": "131",
+        "year": "2022"
+    },
+    "Liu2021Swin": {
+        "author": "Liu, Ze and Lin, Yutong and Cao, Yue and Hu, Han and Wei, Yixuan and Zhang, Zheng and Lin, Stephen and Guo, Baining",
+        "booktitle": "Proceedings of the IEEE/CVF International Conference on Computer Vision",
+        "doi": "10.1109/ICCV48922.2021.00984",
+        "keywords": "vision transformer, hierarchical, shifted windows, segmentation backbone",
+        "pages": "10012--10022",
+        "series": "ICCV",
+        "title": "Swin Transformer: Hierarchical Vision Transformer using Shifted Windows",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1109/ICCV48922.2021.00984",
+        "year": "2021"
+    },
+    "Roggiolani2022Hierarchical": {
+        "author": "Roggiolani, Giacomo and Sodano, Marco and Guadagnino, Tommaso and Magistri, Francesco and Behley, Jens and Stachniss, Cyrill",
+        "journal": "arXiv preprint arXiv:2210.07879",
+        "keywords": "hierarchical segmentation, plant phenotyping, instance segmentation, agricultural imaging",
+        "title": "Hierarchical Approach for Joint Semantic, Plant Instance, and Leaf Instance Segmentation in the Agricultural Domain",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2210.07879",
+        "year": "2022"
+    },
+    "Ward2020Scalable": {
+        "author": "Ward, Daniel and Moghadam, Peyman",
+        "journal": "arXiv preprint arXiv:2003.10757",
+        "keywords": "domain adaptation, transfer learning, cross-species, plant phenotyping",
+        "title": "Scalable Learning for Bridging the Species Gap in Image-Based Plant Phenotyping",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2003.10757",
+        "year": "2020"
+    },
+    "Xie2021SegFormer": {
+        "author": "Xie, Enze and Wang, Wenhai and Yu, Zhiding and Anandkumar, Anima and {Alvarez Jose M. and Luo, Ping",
+        "booktitle": "Advances in Neural Information Processing Systems",
+        "keywords": "semantic segmentation, transformer, MLP decoder, efficiency",
+        "pages": "12077--12090",
+        "series": "NeurIPS",
+        "title": "SegFormer: Simple and Efficient Design for Semantic Segmentation with Transformers",
+        "type": "inproceedings",
+        "url": "https://proceedings.neurips.cc/paper/2021/hash/2dfb8fde6e1f4b78a302397c0b234cfa-Abstract.html",
+        "volume": "34",
+        "year": "2021"
+    }
+}});
